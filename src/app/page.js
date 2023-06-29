2a0ac208ca5d3f3d04bd3181534d4c72045208fd
Archishma Goli <archishma.goli@gmail.com>
2023-06-29 17:43:20 -0400

    remove projects page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import Nav_bar from "./components/navbar";
 import Fader from "./components/header";
-import Project_Overview from './components/projects';
 
 export default function Home() {
   return (
     <>
       <Nav_bar />
       <Fader />
-      <Project_Overview />
     </>
   );
 }
