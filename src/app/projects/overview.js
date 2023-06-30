b2853b185ad61f1fe90baaa9b3c2f03192074a6d
Archishma Goli <archishma.goli@gmail.com>
2023-06-30 01:18:10 -0400

    apostrophes

--- src/app/projects/overview.js
+++ src/app/projects/overview.js
@@ -5,22 +5,23 @@ export default function Overview() {
     const [isVisible, setVisible] = useState(false);
 
     const domRef = useRef();
+    current = domRef.current;
 
     useEffect(() => {
         const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => entry.isIntersecting && setVisible(true));
         });
 
-        observer.observe(domRef.current);
+        observer.observe(current);
 
-        return () => observer.unobserve(domRef.current);
+        return () => observer.unobserve(current);
     }, []);
 
     return (
         <>
             <div id="projects-container" ref={ domRef } className={ `fade-in-section ${ isVisible ? 'is-visible' : '' }` }>
                 <h1 className="heading general-text"><b>My Projects</b></h1>
-                <h4 className="description general-text">Here's what I've worked on so far!</h4>
+                <h4 className="description general-text">Here&#39;s what I&#39;ve worked on so far!</h4>
             </div>
         </>
     );
