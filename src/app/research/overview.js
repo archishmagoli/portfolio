5d51795903d30e48580c9298643fa1fbe20be409
Archishma Goli <archishma.goli@gmail.com>
2023-07-03 17:24:44 -0400

    ESLint fixes

--- src/app/research/overview.js
+++ src/app/research/overview.js
@@ -26,16 +26,16 @@ export default function Overview() {
                 <h3 className="general-text"><b>Georgia Institute of Technology</b></h3>
                 <h5 className="general-text"><b><a href="http://tid.gatech.edu/">Technologies & International Development Lab:</a> Healthcare Nudge Messaging Initiative</b></h5>
                 <ul>
-                    <li className="course-name">I'm collaborating with current PhD and Master's students to conduct <b>sentiment analysis</b> and <b>Natural Language Processing</b> on social media data to provide key insights to nudge authors (members who draft text message "nudges" to inform the general population).</li>
+                    <li className="course-name">I&#39;m collaborating with current PhD and Master&#39;s students to conduct <b>sentiment analysis</b> and <b>Natural Language Processing</b> on social media data to provide key insights to nudge authors (members who draft text message &#34;nudges&#34; to inform the general population).</li>
                     <li className="course-name">The project is a joint collaboration between <b>Georgia Tech</b>, <b>Emory University</b>, and the <b>Morehouse School of Medicine.</b></li>
-                    <li className="course-name">The current goal I'm pursuing is to come up with a <b>dashboard of insights</b> that clearly displays the most important information coming from health-related posts from platforms such as Twitter, Instagram, and Facebook.</li>
+                    <li className="course-name">The current goal I&#39;m pursuing is to come up with a <b>dashboard of insights</b> that clearly displays the most important information coming from health-related posts from platforms such as Twitter, Instagram, and Facebook.</li>
                     <li className="course-name">Within this broader goal is a commitment to publish a <b>retrospective analysis</b> study in the coming months, through the lens of COVID-19 sentiment from 2020 to 2023.</li>
                 </ul>
 
                 <h5 className="general-text"><b>Vertically Integrated Projects (VIP): <a href="https://www.vip.gatech.edu/teams/vp3">Smart Stadium</a></b></h5>
                 <div className="general-text">
                     <ul>
-                        <li className="course-name">I'm a current member of the SensorNets subteam, and I work on <b>hardware-software interaction</b> with tools such as AWS, MSP-432 microcontroller series.</li>
+                        <li className="course-name">I&#39;m a current member of the SensorNets subteam, and I work on <b>hardware-software interaction</b> with tools such as AWS, MSP-432 microcontroller series.</li>
                         <li className="course-name">Some of my ongoing projects include <b>power testing</b> and the deployment of <b>remote software updates.</b></li>
                         <li><a>Publications:</a>
                             <ul>
